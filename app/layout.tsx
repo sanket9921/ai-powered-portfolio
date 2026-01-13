566fc77f2cb6f4df37750e7474c8cded05bb82b6
Sanket Gaikwad <sanketlike63@gmail.com>
2026-01-13 21:17:12 +0530

    added note working progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
-// app/layout.tsx
 'use client';
 
 import './globals.css';
 import { ThemeProvider } from 'next-themes';
+import PortfolioIntroModal from '@/components/PortfolioIntroModal';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          <PortfolioIntroModal />
           {children}
         </ThemeProvider>
       </body>
